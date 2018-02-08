1530648721bfcf4af35fa4abe5dd08f96f32866b
e-imad <36279857+e-imad@users.noreply.github.com>
2018-02-08 21:48:05 +0000

    Update Don't Panic.py

--- Don't Panic.py
+++ Don't Panic.py
@@ -1,6 +1,18 @@
 a=[]
-n,w,z,m,p,c,d,f=[int(i) for i in input().split()]
-for i in range(f):a.append([int(j)for j in input().split()])
-a.append([m,p])
-a.sort(key=lambda x:x[0])
-while 1:c,d,r=input().split();p=int(d);y=a[int(c)][1];print("BLOCK"if y>p and r=="LEFT"or y<p and r=="RIGHT"else"WAIT")
+b=[]
+n,w,nb,ef,ep,c,d,f=[int(i) for i in input().split()]
+for i in range(f):
+ x, y = [int(j) for j in input().split()]
+ a.append(x)
+ b.append(y)
+a.append(ef)
+b.append(ep)
+while True:  
+ c,d,dr = input().split()
+ cf=int(c)
+ cp=int(d)
+ if cf!=-1:
+  y=b[a.index(cf)]
+  if y>cp and dr=="LEFT" or y<cp and dr=="RIGHT":print("BLOCK")
+  else:print("WAIT")
+ else:print("WAIT")
